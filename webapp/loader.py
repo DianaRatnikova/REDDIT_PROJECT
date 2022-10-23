1e9ef0ea7d1fc8bbccdffea1aff02466bbf85daa
DianaRatnikova <Diana_r@bk.ru>
2022-10-24 02:17:17 +0300

    Убрала добавление записи, если в ней не произошло изменений

--- webapp/loader.py
+++ webapp/loader.py
@@ -5,7 +5,7 @@ import time
 import webapp.config_auth
 from webapp.print_reddit_data import mkdir_for_results
 from webapp.models import Top_subreddits, Comments, Comments_edit
-from sqlalchemy import distinct
+#from sqlalchemy import distinct
 import logging
 
 def read_top_subreddit_csv(filename):
@@ -41,12 +41,20 @@ def read_comments_edits_csv(filename):
 def save_top_subreddit(all_data):
     processed = []
     top_subreddit_unique = []
+
+    query_url = db_session.query(Top_subreddits.url_subreddit).distinct()
+    urls = []
+    for url in query_url:
+        urls.append(url[0])
+
     for row in all_data:
         if row['url_subreddit'] not in processed:
             top_subreddit = {'subreddit': row['subreddit'],'author_subreddit': row['author_subreddit'],
             'url_subreddit': row['url_subreddit'], 'title': row['title'], }
-            top_subreddit_unique.append(top_subreddit)
-            processed.append(top_subreddit['url_subreddit'])
+
+            if not urls or top_subreddit['url_subreddit'] not in urls:
+                top_subreddit_unique.append(top_subreddit)
+                processed.append(top_subreddit['url_subreddit'])
 # return_defaults=True говорит bulk_insert_mappings, 
 # что когда база присвоит компаниям id, их нужно добавить в top_subreddit_unique.
     db_session.bulk_insert_mappings(Top_subreddits, top_subreddit_unique, return_defaults=True)
@@ -66,16 +74,23 @@ def get_top_subreddit_id(url_subreddit, top_subreddit_unique):
 def save_comments(all_data, top_subreddit_unique):
     processed = []
     comments_unique = []
+
+    query_id = db_session.query(Comments.identificator).distinct()
+    identificators = []
+    for id in query_id:
+        identificators.append(id[0])
+
     for row in all_data:
         if row['identificator'] not in processed:
-        #    logging.info('22')
             comment = {'author_comment': row['author_comment'], 'body': row['body'], 'mood': '', 'url_comment': row['url_comment'],
             'nesting': row['nesting'], 'identificator': row['identificator']
               }
-          #  logging.info('33')
             comment['top_subreddit_id'] = get_top_subreddit_id(row['url_comment'], top_subreddit_unique)
-            comments_unique.append(comment)
-            processed.append(row['identificator'])
+
+            if not identificators or comment['identificator'] not in identificators:
+                comments_unique.append(comment)
+                processed.append(row['identificator'])
+
     db_session.bulk_insert_mappings(Comments, comments_unique, return_defaults=True)
     db_session.commit()
     return comments_unique
@@ -84,32 +99,34 @@ def save_comments(all_data, top_subreddit_unique):
 def save_comments_edits(all_data,top_subreddit_unique):
     processed = []
     comments_edits_unique = []
-    query = db_session.query(Comments_edit.identificator_comment).distinct()
- 
+
+    query_id = db_session.query(Comments_edit.identificator_comment).distinct()
     identificators = []
-    for id in query:
+    for id in query_id:
         identificators.append(id[0])
-    print(f"{identificators = }")
+
+    query_num = db_session.query(Comments_edit.edition_num).distinct()
+    numbers_of_edition = []
+    for num in query_num:
+        numbers_of_edition.append(num[0])
 
     for row in all_data:
         if row['edition_num'] not in processed:
             comment_edit = {'body': row['body'], 'mood': '', 'identificator_comment': row['identificator_comment'],
             'edition_num': row['edition_num'],'url_comment': row['url_comment'],
             }
+            comment_edit['top_subreddit_id'] = get_top_subreddit_id(row['url_comment'], top_subreddit_unique)
 
-            if not query.all():
-                    comment_edit['top_subreddit_id'] = get_top_subreddit_id(row['url_comment'], top_subreddit_unique)
+            if not identificators:
                     comments_edits_unique.append(comment_edit)
                     processed.append(row['edition_num'])
 
-            for id in query.all():
+            for id in identificators:
                 if comment_edit['identificator_comment'] == id:
-                    if comment_edit['edition_num'] in db_session.query(Comments_edit.edition_num):
-                        print("No more edited!")
+                    if int(comment_edit['edition_num']) in numbers_of_edition:
+                        print(f"No more edited!  {comment_edit['edition_num'] = }")
                     else:
-                        print("Edited!")
-     #       comments_edits_unique['identificator_comment'] = get_top_subreddit_id(row['url_comment'], top_subreddit_unique)
-                        comment_edit['top_subreddit_id'] = get_top_subreddit_id(row['url_comment'], top_subreddit_unique)
+                        print(f"Edited! {comment_edit['edition_num'] = }")
                         comments_edits_unique.append(comment_edit)
                         processed.append(row['edition_num'])
     db_session.bulk_insert_mappings(Comments_edit, comments_edits_unique, return_defaults=True)
